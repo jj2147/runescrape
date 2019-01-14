46902c411f60c19b3646752bfc74d15fed3b8f86
jason jia <jasonpjia@gmail.com>
2019-01-13 21:47:54 -0600

    .

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db = require("./models");
 
 var app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
